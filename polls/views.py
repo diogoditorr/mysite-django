74cd4278fe7d5348f3145d2b47cdd9ea8ec2595b
Diego Oliveira <35296262+diogoditorr@users.noreply.github.com>
2021-09-13 10:40:57 -0300

    tutorial 05 - automated testing

--- polls/views.py
+++ polls/views.py
@@ -41,7 +41,7 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
 
-def vote(request, question_id):
+def vote(request: HttpRequest, question_id: int):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
